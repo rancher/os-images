e6276030aafcf25722a2e0d048350128db49c20d
Ivan Mikushin <i.mikushin@gmail.com>
2015-06-02 19:29:25 +0500

    cleanup: simplify and speedup

--- build.sh
+++ build.sh
@@ -3,22 +3,9 @@ set -e
 
 cd $(dirname $0)
 
-export DOCKER_IMAGE=rancher-os-images-build
-
 source ./scripts/version
+export VERSION
 
-./scripts/ci
-mkdir -p dist/artifacts
-docker run --rm -it -e CHOWN_ID=$(id -u) -v $(pwd)/dist:/source/target $DOCKER_IMAGE
-
-# Stupidest argparse ever
-if echo "$@" | grep -q -- '--images'; then
-    ./scripts/build-extra-images
-fi
-
-# And again
-if echo "$@" | grep -q -- '--push'; then
-    docker push rancher/ubuntuconsole:${VERSION}
-fi
-
-#ls -l dist/artifacts
+./scripts/clean
+./scripts/download
+./scripts/build-images
