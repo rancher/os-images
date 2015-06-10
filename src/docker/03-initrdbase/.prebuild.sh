6dcbe4f6cf2bda7cbff81a2827ce9fa785f3e0f3
Ivan Mikushin <i.mikushin@gmail.com>
2015-06-10 22:54:29 +0500

    rancher/os-initrdbase

---
+++ src/docker/03-initrdbase/.prebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)
+rm -rf ./build
+mkdir -p ./build/initrd
+
+tar xf ./../00-rootfs/build/dist/rootfs-static.tar -C ./build/initrd --strip-components=2 ./bin/busybox
+tar xf ./../00-rootfs/build/dist/rootfs-static.tar -C ./build/initrd --strip-components=3 ./usr/sbin/xtables-multi
+cp ./../02-docker/build/ca-certificates.crt ./build/initrd/ca.crt
