602258907744dd5998a4bebc619696e7035c61f0
Ivan Mikushin <i.mikushin@gmail.com>
2015-07-21 13:13:10 +0500

    use ros dev to find cloud-config device

--- src/docker/02-cloudinit/cloud-init.sh
+++ src/docker/02-cloudinit/cloud-init.sh
@@ -2,12 +2,12 @@
 set -x -e
 
 MOUNT_POINT=/media/config-2
-CONFIG_DEV=$(blkid | grep -- 'LABEL="config-2"' | cut -f1 -d:)
+CONFIG_DEV=$(ros dev "LABEL=config-2")
 
 mkdir -p ${MOUNT_POINT}
 
 if [ -e "${CONFIG_DEV}" ]; then
-    mount ${CONFIG_DEV} ${MOUNT_POINT}
+    mount -t iso9660,vfat ${CONFIG_DEV} ${MOUNT_POINT}
 else
     mount -t 9p -o trans=virtio,version=9p2000.L config-2 ${MOUNT_POINT} 2>/dev/null || true
 fi
