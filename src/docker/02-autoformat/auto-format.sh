f9bbcfa3514df468c6bb3e51eafffba6622c825e
Ivan Mikushin <i.mikushin@gmail.com>
2015-07-21 19:36:12 +0500

    impl auto-formatter drive selection logic in the container
    Auto-formatter is only going to be run by RancherOS if `rancher.state.autoformat` is not empty (e.g. set in kernel params, in ISO)
    
    By default, we will not try to auto-format disks beginning with 1MB zeros. To override, `rancher.state.formatzero` should be set to `true`.

--- src/docker/02-autoformat/auto-format.sh
+++ src/docker/02-autoformat/auto-format.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
-
 set -ex
 
-if [ -n "$1" ]; then
-    # Test for our magic string (it means that the disk was made by ./boot2docker init)
-    HEADER=`dd if=$1 bs=1 count=${#MAGIC} 2>/dev/null`
+MAGIC=${MAGIC:-"boot2docker, please format-me"}
 
-    if [ "$HEADER" = "$MAGIC" ]; then
-        # save the preload userdata.tar file
-	dd if=$1 of=/userdata.tar bs=1 count=8192
-    fi
+AUTOFORMAT=${AUTOFORMAT:-"/dev/sda /dev/vda"}
+DEVS=(${AUTOFORMAT})
+FORMATZERO=${FORMATZERO:-false}
 
-    mkfs.ext4 -L RANCHER_STATE $1
+for dev in ${DEVS[@]}; do
+    if [ -b "${dev}" ]; then
 
-    if [ -e "/userdata.tar" ]; then
-        mount -t ext4 $1 /var/
-        mkdir -p /var/lib/rancher/conf/cloud-config.d
-        echo $(tar -xvf /userdata.tar)
-        AUTHORIZED_KEY1=$(cat /.ssh/authorized_keys)
-        AUTHORIZED_KEY2=$(cat /.ssh/authorized_keys2)
-        tee /var/lib/rancher/conf/cloud-config.d/machine.yml << EOF
+        # Test for our magic string (it means that the disk was made by ./boot2docker init)
+        HEADER=`dd if=${dev} bs=1 count=${#MAGIC} 2>/dev/null`
+    
+        if [ "$HEADER" = "$MAGIC" ]; then
+            # save the preload userdata.tar file
+            dd if=${dev} of=/userdata.tar bs=1 count=8192
+        elif [ "${FORMATZERO}" != "true" ]; then
+            # do not try to guess whether to auto-format a disk beginning with 1MB filled with 00
+            continue
+        elif ! od -A d -N 1048576 ${dev} | head -n 3 | diff ./od-1m0 - >/dev/null 2>&1; then
+            # do not auto-format if the disk does not begin with 1MB filled with 00
+            continue
+        fi
+    
+        mkfs.ext4 -L RANCHER_STATE ${dev}
+    
+        if [ -e "/userdata.tar" ]; then
+            mount -t ext4 ${dev} /var/
+            mkdir -p /var/lib/rancher/conf/cloud-config.d
+            echo $(tar -xvf /userdata.tar)
+            AUTHORIZED_KEY1=$(cat /.ssh/authorized_keys)
+            AUTHORIZED_KEY2=$(cat /.ssh/authorized_keys2)
+            tee /var/lib/rancher/conf/cloud-config.d/machine.yml << EOF
 #cloud-config
 
 rancher:
@@ -33,15 +46,18 @@ rancher:
     address: 127.0.0.1/8
 
 ssh_authorized_keys:
- - $AUTHORIZED_KEY1
- - $AUTHORIZED_KEY2 
+ - ${AUTHORIZED_KEY1}
+ - ${AUTHORIZED_KEY2} 
 
 users:
  - name: docker
    ssh_authorized_keys:
-   - $AUTHORIZED_KEY1
-   - $AUTHORIZED_KEY2
+   - ${AUTHORIZED_KEY1}
+   - ${AUTHORIZED_KEY2}
 EOF
-    fi
-fi
+        fi
 
+        # do not check another device
+        break
+    fi
+done
