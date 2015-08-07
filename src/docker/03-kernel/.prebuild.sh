4ce7b071bd359beae6359f4d15919efd29ec2e23
Ivan Mikushin <i.mikushin@gmail.com>
2015-08-07 19:55:36 +0500

    rm os-kernel and os-initrdbase images

--- src/docker/03-kernel/.prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd $(dirname $0)
-rm -rf ./build
-mkdir -p ./build/kernel
-
-cp ./../00-rootfs/build/dist/kernel/boot/vmlinuz* ./build/kernel/vmlinuz
-cp -a ./../00-rootfs/build/dist/kernel/lib ./build/kernel
