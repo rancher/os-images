1e04d1b66b9d7f5a882f5dc79e84f56e6f3b91d0
Darren Shepherd <darren@rancher.com>
2015-08-27 02:04:09 -0700

    Run udev twice in bootstrap

--- src/docker/02-udev/udev.sh
+++ src/docker/02-udev/udev.sh
@@ -7,3 +7,12 @@ fi
 udevd --daemon
 udevadm trigger --action=add
 udevadm settle
+
+if [ "$BOOTSTRAP" = true ]; then
+    # This was needed to get USB devices to fully register
+    # There is probably a better way to do this
+    killall udevd
+    udevd --daemon
+    udevadm trigger --action=add
+    udevadm settle
+fi
