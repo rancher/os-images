b97c69c02067ce8d0278515e28d8864e0ab33216
Ivan Mikushin <i.mikushin@gmail.com>
2015-10-16 14:05:24 +0500

    add stamp file to prevent subsequent runs

--- src/docker/10-resizefs/resizefs.sh
+++ src/docker/10-resizefs/resizefs.sh
@@ -2,9 +2,16 @@
 set -x
 
 DEV=${1}
+STAMP=/var/log/resizefs.done
+
 if [ "$DEV" == "" ]; then
   echo "resizefs.sh: expecting device as the 1st arg"
   exit 1
+fi
+
+if [ -e $STAMP ]; then
+    echo Headers already installed in $DIR
+    exit 0
 fi
 
 if ls ${DEV} >/dev/null 2>&1; then
@@ -22,3 +29,4 @@ EOF
 fi
 
 resize2fs ${DEV}1
+touch $STAMP
