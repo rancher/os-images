29722577f6b42cc4a2bee204668f32d607bc302b
Josh Curl <josh@curl.me>
2016-06-28 13:28:46 -0700

    Allow kernel headers URL to be configurable

--- images/02-headers/headers.sh
+++ images/02-headers/headers.sh
@@ -4,16 +4,16 @@ set -e
 DIR=$(readlink /lib/modules/$(uname -r)/build)
 STAMP=${DIR}/.done
 VER=$(basename $DIR)
-URL=https://github.com/rancher/os-kernel/releases/download/${VER}/build.tar.gz
+KERNEL_HEADERS_URL=${KERNEL_HEADERS_URL:-https://github.com/rancher/os-kernel/releases/download/${VER}/build.tar.gz}
 
 if [ -e $STAMP ]; then
     echo Headers already installed in $DIR
     exit 0
 fi
 
-echo Downloading $URL
+echo Downloading $KERNEL_HEADERS_URL
 mkdir -p $DIR
-wget -O - $URL | gzip -dc | tar xf - -C $DIR
+wget -O - $KERNEL_HEADERS_URL | gzip -dc | tar xf - -C $DIR
 touch $STAMP
 
 echo Headers installed at $DIR
