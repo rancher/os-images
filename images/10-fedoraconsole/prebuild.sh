8be8639ea53ca489d1c637546d8d3c52518709e5
Josh Curl <josh@curl.me>
2016-08-12 11:41:43 -0700

    Remove console.sh and docker-init scripts

--- images/10-fedoraconsole/prebuild.sh
+++ images/10-fedoraconsole/prebuild.sh
@@ -5,7 +5,5 @@ cd $(dirname $0)
 
 rm -rf ./build
 mkdir -p ./build
-cp ./../02-console/console.sh ./build/
-cp ./../02-console/docker-init ./build/
 cp ./../02-console/update-ssh-keys ./build/
 cp ./../02-console/rancheros-install ./build/
